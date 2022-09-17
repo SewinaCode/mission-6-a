252b2b42572bce2edc78aea660d323d5b470a5ed
MissionNick <nickw@missionreadyhq.com>
2022-09-17 20:56:26 +1200

    Changed dockerfile - image 16-17.0 alpine, npm ci and package-lock for clean install and build

--- frontend/src/components/cards.js
+++ frontend/src/components/cards.js
@@ -1,25 +1,22 @@
-// import axios from "axios";
-// import React, { useEffect, useState } from "react";
+import React from "react";
 import Laptop from "../assets/macbook-pro-test.webp";
 
 export default function Cards({
   backgroundColor,
   border,
-  // description,
+  description,
   margin,
-  // name,
+  name,
   padding,
-  // product,
-  // quantity,
-  // imageUrl,
+  product,
+  quantity,
+  imageUrl,
 }) {
   //Put code to transfer data here. use array.map() function - means whenever the database is updated it will create another card with new document from MongoDB.
 
   return (
     <>
-      {tech &&
-        tech.map((tech) => (
-          <div
+         <div
             style={{
               backgroundColor: "white",
               border: "2px solid black",
@@ -27,24 +24,23 @@ export default function Cards({
               padding: "20px 30px",
             }}
           >
-            <img src={Laptop} alt={tech.desc} />
+        <img src={Laptop} alt={imageUrl} />
             <div>
               <h2>
-                <b>Product name</b>: {tech.name}
+                <b>Product name</b>: {product}
               </h2>
             </div>
             <div>
               <h2>
-                <b>Product Description</b>: {tech.desc}
+                <b>Product Description</b>: {description}
               </h2>
             </div>
             <div>
               <h2>
-                <b>Quantity</b>: {tech.quantity}
+                <b>Quantity</b>: {quantity}
               </h2>
             </div>
           </div>
-        ))}
     </>
   );
 }
