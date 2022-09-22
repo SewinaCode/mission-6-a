905a0b530fd2810cd57d62c246079d95d5254534
Sellyna <sellynalky@gmail.com>
2022-09-23 02:09:54 +1200

    adjusted App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/Modal/i);
+  const linkElement = screen.getByText(/Computers/i);
   expect(linkElement).toBeInTheDocument();
 });
