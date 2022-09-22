980f52f03f7f9ef3b6047a71cbd39d47a6b3905e
Sellyna <sellynalky@gmail.com>
2022-09-22 17:25:37 +1200

    fixed errors stopping build in github actions

--- src/components/p-icons.js
+++ src/components/p-icons.js
@@ -1,58 +1,28 @@
-import cpu from "../assets/cpu-icon.png" 
-import gpu from "../assets/gpu.png" 
-import ram from "../assets/ram-memory-icon1.png"
-import screenres from "../assets/screensize.png"
-import camera from "../assets/black-camera-icon 1.png" 
-import monitor from "../assets/computer-monitor-icon.png"
-import wifi from "../assets/wifi.png"
-import orangesale from "../assets/sale-tag.png"
-import battery from "../assets/battery.png"
-import blacktag from "../assets/black-tag.png"
-import ssd from "../assets/SSD.png"
+import cpu from "../assets/cpu-icon.png";
+// import gpu from "../assets/gpu.png"
+import ram from "../assets/ram-memory-icon1.png";
+import screenres from "../assets/screensize.png";
+// import camera from "../assets/black-camera-icon 1.png"
+// import monitor from "../assets/computer-monitor-icon.png"
+// import wifi from "../assets/wifi.png"
+// import orangesale from "../assets/sale-tag.png"
+// import battery from "../assets/battery.png"
+// import blacktag from "../assets/black-tag.png"
+import ssd from "../assets/SSD.png";
 
-import FeatureIcon from "../components/p-feat"
-
-
+import FeatureIcon from "../components/p-feat";
 
 export default function FeatureSpec(features) {
-
-    return (
-        < div className="p-features" >
-            <FeatureIcon
-                image={cpu}
-                name="Processor"
-                spec="Intel Core i7"
-            />
-            <FeatureIcon
-                image={null}
-                name="OS"
-                spec="Windows 11 Pro"
-            />
-            <FeatureIcon
-                image={ssd}
-                name="SSD"
-                spec="1tb"
-            />
-            <FeatureIcon
-                image={ram}
-                name="DDR-4"
-                spec="124gb"
-            />
-            <FeatureIcon
-                image={screenres}
-                name='15.6"'
-                spec="inch"
-            />
-        </div>
-    )
+  return (
+    <div className="p-features">
+      <FeatureIcon image={cpu} name="Processor" spec="Intel Core i7" />
+      <FeatureIcon image={null} name="OS" spec="Windows 11 Pro" />
+      <FeatureIcon image={ssd} name="SSD" spec="1tb" />
+      <FeatureIcon image={ram} name="DDR-4" spec="124gb" />
+      <FeatureIcon image={screenres} name='15.6"' spec="inch" />
+    </div>
+  );
 }
-
-
-
-
-
-
-
 
 /*
 {
@@ -71,6 +41,3 @@ export default function FeatureSpec(features) {
         {ssd:"../assets/SSD.png"}
     ]
 */
-
-
-
